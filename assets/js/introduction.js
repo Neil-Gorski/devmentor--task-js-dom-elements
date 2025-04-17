54327965bd10fb8d2581d739e702be77f7cb2d9e
Neil Gorski <neil.gorski@hotmail.ch>
2025-04-17 23:57:02 +0200

    Task-1 done

---
+++ assets/js/introduction.js
@@ -0,0 +1,47 @@
+const tooltipList = document.querySelectorAll(".tooltip");
+const tooltipArr = Array.from(tooltipList);
+console.log(typeof tooltipArr, tooltipArr);
+
+if (tooltipArr.length != 0) {
+  for (element of tooltipArr) {
+    const stats = {
+      classList: [...element.classList],
+      url: element.dataset.url,
+      tooltipType: element.dataset.tooltipType,
+      tooltipContent: element.dataset.tooltipContent,
+      text: element.innerText,
+    };
+
+    element.replaceWith(createTooltip(stats));
+  }
+}
+
+function createTooltip(obj) {
+  const tooltipSpan = document.createElement("span");
+  if (obj.classList.length != 0) {
+    for (classElement of obj.classList) {
+      tooltipSpan.classList.add(classElement);
+    }
+  }
+  const tooltipAnchor = document.createElement("a");
+  tooltipAnchor.innerText = obj.text;
+  tooltipAnchor.href = obj.url;
+  tooltipSpan.appendChild(tooltipAnchor);
+
+  const tooltipSpanContent = document.createElement("span");
+  tooltipSpanContent.classList.add("tooltip__box");
+  if (obj.tooltipType === "text") {
+    tooltipSpanContent.classList.add("tooltip__box--text");
+    tooltipSpanContent.innerText = obj.tooltipContent;
+  } else if (obj.tooltipType === "image") {
+    tooltipSpanContent.classList.add("tooltip__box--img");
+    const tooltipImg = document.createElement("img");
+    tooltipImg.classList.add("tooltip__image");
+    tooltipImg.src = obj.tooltipContent;
+    tooltipSpanContent.appendChild(tooltipImg);
+  }
+
+  tooltipSpan.appendChild(tooltipSpanContent);
+  console.log(tooltipSpan);
+  return tooltipSpan;
+}
